b59202bcf6866aa18a4afb0719e8b8dad6f051e6
Rakhi Keshri <rakhi.kumaribce@gmail.com>
2024-05-25 12:32:00 +0530

    changes

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: true,
-    credentials: true
+    // origin: true,
+    origin: ['https://mern-notes-frontend-sk49.vercel.app/'],
+    credentials: true,
+    methods:["GET","POST","PUT","DELETE"]
 }))
 
 // Connect to db
